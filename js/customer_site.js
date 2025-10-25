ab863fbd09822774437b7748dd7bde8d03ab03fe
mx-love <155623834+mx-love@users.noreply.github.com>
2025-10-25 18:14:33 +0800

    Update customer_site.js

--- js/customer_site.js
+++ js/customer_site.js
@@ -1,11 +1,17 @@
-const CUSTOMER_SITES = {
-   qiqi: {
-        api: 'https://www.qiqidys.com/api.php/provide/vod',
-        name: '七七资源',
+const ENABLE_CUSTOMER_SITES = false;
 
-// 调用全局方法合并
-if (window.extendAPISites) {
-    window.extendAPISites(CUSTOMER_SITES);
-} else {
-    console.error("错误：请先加载 config.js！");
+if (ENABLE_CUSTOMER_SITES) {
+    const CUSTOMER_SITES = {
+        qiqi: {
+            api: 'https://www.qiqidys.com/api.php/provide/vod',
+            name: '七七资源',
+        }
+    };
+
+    // 调用全局方法合并
+    if (window.extendAPISites) {
+        window.extendAPISites(CUSTOMER_SITES);
+    } else {
+        console.error("错误：请先加载 config.js！");
+    }
 }
